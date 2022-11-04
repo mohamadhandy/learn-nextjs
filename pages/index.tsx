3a0f8c5f53fa870a49c1c18169d9fa849b9ce3dd
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-04 14:03:38 +0700

    basic change page using link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <h1>Welcome Handy Nugraha</h1>
+    <>
+      <header>
+        <ul>
+          <li><Link href={"/users"}>Users</Link></li>
+          <li><Link href={"/blog"}>Blog</Link></li>
+        </ul>
+      </header>
+      <h1>Welcome Mohamad Handy Nugraha</h1>
+    </>
   )
 }
