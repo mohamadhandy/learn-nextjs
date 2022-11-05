0891e99179d8a75d78127b6c9f591d658e2cd92a
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-06 06:44:00 +0700

    create autoredirection (before this create 404)

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 export default function Custom404() {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/")
+    }, 2000)
+  }, [router])
+  
   return (
     <div className="title-notfound">
       <h1>Oops!</h1>
