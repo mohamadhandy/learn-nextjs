ff8eb1f0cb7c5a43dead63b3c8175a7c4550e483
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-06 06:38:06 +0700

    create autoredirection

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Custom404() {
+  return (
+    <div className="title-notfound">
+      <h1>Oops!</h1>
+      <h1>Halaman yang anda cari tidak ditemukan</h1>
+    </div>
+  )
+}
