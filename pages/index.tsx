cc57a9067887a242a923637a2b69c0323e1c94b0
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-06 07:48:30 +0700

    add Image using tag Image from nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,50 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
-import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Layout pageTitle="Home page">
+        <Image src="/1.PNG" width={200} height={200} alt="Test image" />
         <h1 className={styles["title-homepage"]}>
           Welcome Mohamad Handy Nugraha
         </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <Image src="/78.PNG" width={200} height={200} alt="Test image" />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio architecto nemo natus explicabo eaque. Vitae incidunt tenetur rerum nam sapiente.</p>
+        <Image src="/asap.PNG" width={200} height={200} alt="Test image" />
       </Layout>
     </>
   );
