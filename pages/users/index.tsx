ca98b476891d0f19c68f1e303c07b9256f610449
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-07 09:49:10 +0700

    learn getStaticProps in nextjs

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 
-export default function User() {
+interface UsersProps {
+  dataUsers: Array<any>
+}
+export default function User(props: UsersProps) {
+  const { dataUsers } = props
   return (
     <Layout pageTitle="Users page">
       <p>User index root page</p>
+      <ul>
+        {dataUsers?.map((user: any, index: any) => {
+          return (
+            <li key={index}>
+              {user.name} - {user.email}
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const users = await (await fetch("https://jsonplaceholder.typicode.com/users")).json()
+  return {
+    props: {
+      dataUsers: users
+    }
+  }
 }
