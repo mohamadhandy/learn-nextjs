bff0e5db3d9a3a1a661e40f0a51607e90bae9f43
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-07 10:47:06 +0700

    learn getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,39 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Blog.module.css"
 
-export default function Blog() {
+
+interface PostProps {
+  id: number
+  title: string
+  body: string
+}
+interface BlogProps {
+  dataBlog: PostProps[]
+}
+
+export default function Blog(props: BlogProps) {
+  const { dataBlog } = props;
   return (
     <Layout pageTitle="Blog page">
-      <p className="title">Sample p tag on blog page</p>
+      {dataBlog?.map((blog: PostProps) => {
+        return (
+          <div key={blog.id} className={styles.card}>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const dataBlog = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+  ).json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
